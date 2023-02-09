1231bae3447598ba36af76c05c13397e38ac240f
H25555 <124329015+H25555@users.noreply.github.com>
2023-02-09 09:17:06 +0700

    Add files via upload

---
+++ TH3/th3_3.js
@@ -0,0 +1,9 @@
+let a= prompt('Nhập a')
+let b= prompt('Nhập b')
+parseFloat('a')
+parseFloat('b')
+if (b%a==0) {
+    document.write('a la boi so cua b' )
+} else {
+    document.write('a khong phai la boi so cua b')
+}
